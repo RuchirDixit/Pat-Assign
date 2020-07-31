92f25a20e0a3fc9b5c0d3e05955c4529a8d12efb
Ruchir Dixit <ruchirtd96@gmail.com>
2020-07-31 12:28:56 +0530

    Changes made

--- userRegisterProg.sh
+++ userRegisterProg.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #UC1
 
-#read -p "Enter First name to check validity:" firstName
-#pat="^([[:upper:]]{1})[a-zA-Z]{2,}$"
+read -p "Enter First name to check validity:" firstName
+pat="^([[:upper:]]{1})[a-zA-Z]{2,}$"
 if [[ $firstName =~ $pat ]]
 then
 	echo Valid
@@ -11,8 +11,8 @@ else
 fi
 
 #UC2
-#read -p "Enter Last name to check validity:" lastName
-#pat="^([[:upper:]]{1})[a-zA-Z]{2,}$"
+read -p "Enter Last name to check validity:" lastName
+pat="^([[:upper:]]{1})[a-zA-Z]{2,}$"
 if [[ $lastName =~ $pat ]]
 then
    echo Valid
@@ -21,9 +21,19 @@ else
 fi
 
 #UC3
-read -p "Enter email id to check validity:" emailId
-pat="^[a-zA-Z]+([.+#_][a-zA-Z0-9]+)*@[a-zA-Z]+\.[a-zA-Z]{2}$"
-if [[ $emailId =~ $pat ]]
+read -p "Enter email to check validity:" emailID
+pat="^[a-zA-Z]+([.#&_][a-zA-Z0-0]+)*@[a-zA-Z]+\.[a-zA-Z]{2}([.][a-zA-Z]+)*$"
+if [[ $emailID =~ $pat ]]
+then
+   echo Valid
+else
+   echo Invalid
+fi
+
+#UC4
+read -p "Enter mob number to check validity:" phoneNumber
+pat="^(91)[ ][0-9]{10}$"
+if [[ $phoneNumber =~ $pat ]]
 then
    echo Valid
 else
